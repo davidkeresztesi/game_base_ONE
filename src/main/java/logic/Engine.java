676decc971f39b429a3781996ee61c13f6aa2994
davidkeresztesi <keresztesidavid47@gmail.com>
2022-04-18 20:49:03 +0200

    mapReader() fix

--- src/main/java/logic/Engine.java
+++ src/main/java/logic/Engine.java
@@ -119,16 +119,21 @@ public class Engine {
         }
     }
 
+
+    //https://www.youtube.com/watch?v=rWzINXeC0lY
+
     private List<String> mapReader(String fileName) {
         String inPath = DATA_PATH + fileName + TXT;
         List<String> textHolderList = new ArrayList<>();
         try {
+
             BufferedReader reader = new BufferedReader(new FileReader(inPath));
             String line;
             while ((line = reader.readLine()) != null) {
                 textHolderList.add(line);
             }
             reader.close();
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
